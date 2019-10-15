e473b221aa97342a9b72ba1c6ea4046247726311
James Wang <wang935280835@qq.com>
2019-10-15 22:10:27 +0800

    init

--- cloud-config/src/main/java/config/ConfigServerApp.java
+++ cloud-config/src/main/java/config/ConfigServerApp.java
@@ -8,9 +8,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableConfigServer
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigServerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApp.class, args);
--- cloud-core/src/main/java/com/james/core/App.java
+++ cloud-core/src/main/java/com/james/core/App.java
@@ -2,11 +2,13 @@ package com.james.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class App {
     public static void main(String[] args) {
 
